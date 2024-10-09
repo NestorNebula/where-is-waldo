96cde9ea98eff0c044656366be9d2a0f20c24baa
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-09 15:36:24 +0200

    feat: add controllers in routes

--- server/routes/photo.js
+++ server/routes/photo.js
@@ -1,4 +1,10 @@
 const { Router } = require('express');
 const router = Router();
+const controller = require('../controllers/controller').photo;
+
+router.get('/', controller.getPhotos);
+router.get('/:photoId/rounds', controller.getPhotoRounds);
+router.post('/', controller.postPhoto);
+router.put('/:photoId', controller.updatePhoto);
 
 module.exports = router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,4 +1,9 @@
 const { Router } = require('express');
 const router = Router();
+const controller = require('../controllers/controller').user;
+
+router.get('/:userId', controller.getUser);
+router.post('/', controller.postUser);
+router.put('/:userId', controller.updateUser);
 
 module.exports = router;
