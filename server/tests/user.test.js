8fac98fbf76f9497f7a2d1f1a30f6042566477fe
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-09 17:48:34 +0200

    feat: add first test for user

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -1,1 +1,28 @@
+const { request, app } = require('./setup');
+const router = require('../routes/routes').user;
+const { getFakeUser } = require('../helpers/faker');
 
+app.use('/', router);
+
+const mockUser = getFakeUser();
+
+jest.mock('../models/queries', async () => {
+  return {
+    ...jest.requireActual('../models/queries'),
+    getUser: () => {
+      return {
+        mockUser,
+      };
+    },
+  };
+});
+
+describe('user route', () => {
+  it('returns user informations', (done) => {
+    request(app)
+      .get(`/${mockUser.id}`)
+      .then((res) => {
+        expect(res, done).toEqual(mockUser);
+      });
+  });
+});
