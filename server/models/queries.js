e55933d0588bc84f13c8220b26a58989bd6cb23b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-09 17:52:33 +0200

    feat: add getUser query

--- server/models/queries.js
+++ server/models/queries.js
@@ -4,8 +4,16 @@ const prisma = new PrismaClient();
 
 // User queries
 
+const getUser = async (id) => {
+  const user = await prisma.user.findUnique({
+    where: { id },
+    include: { rounds: true },
+  });
+  return user;
+};
+
 // Photo queries
 
 // Round queries
 
-module.exports = {};
+module.exports = { getUser };
