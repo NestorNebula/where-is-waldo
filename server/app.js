2ec1cd369888079cf25d388e5ed16063eb605162
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-09 15:12:05 +0200

    feat: create routes for the app

--- server/app.js
+++ server/app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
+const router = require('./routes/routes');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/users', router.user);
+app.use('/photos', router.photo);
+app.use('/rounds', router.round);
 
 app.listen(process.env.PORT, () => `App running on PORT ${process.env.PORT}.`);
---
+++ server/routes/routes.js
@@ -0,0 +1,5 @@
+const user = require('./user');
+const photo = require('./photo');
+const round = require('./round');
+
+module.exports = { user, photo, round };
