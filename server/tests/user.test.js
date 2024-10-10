3656c5b6d449715bfe887c6f77a3261de9407ca9
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-10 10:44:22 +0200

    test: add test for inexistant user

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -5,6 +5,7 @@ const { getFakeUser } = require('../helpers/faker');
 app.use('/', router);
 
 const mockUser = getFakeUser();
+const fakeUser = getFakeUser();
 
 jest.mock('../models/queries', () => {
   return {
@@ -28,5 +29,9 @@ describe('user route', () => {
       .then((res) => {
         expect(res.body.user).toEqual(mockUser);
       });
+  });
+
+  it("returns 404 when user doesn't exist", (done) => {
+    request(app).get(`/${fakeUser.id}`).expect(404, done);
   });
 });
