f523c5be8659fc950af9b6982a260622ce69136f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-10 17:23:00 +0200

    feat: add postRound function

--- server/controllers/round.js
+++ server/controllers/round.js
@@ -1,4 +1,12 @@
-const postRound = () => {};
+const prisma = require('../models/queries');
+
+const postRound = async (req, res) => {
+  const user = await prisma.getUser(+req.body.userId);
+  const photo = await prisma.getPhoto(+req.body.photo);
+  if (!user || !photo) return res.sendStatus(400);
+  await prisma.createRound(user.id, photo.id);
+  res.sendStatus(201);
+};
 
 const updateRound = () => {};
 
