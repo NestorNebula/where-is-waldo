4bc9caa20cd16f82c104453bfd76f0f2ce8d6707
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-10 17:37:13 +0200

    feat: add tests for updating round

--- server/tests/round.test.js
+++ server/tests/round.test.js
@@ -1,10 +1,16 @@
 const { request, app } = require('./setup');
 const router = require('../routes/routes').round;
-const { getFakeUser, getFakePhoto, getFakeRound } = require('../helpers/faker');
+const {
+  getFakeUser,
+  getFakePhoto,
+  getFakeRound,
+  getFakeOngoingRound,
+} = require('../helpers/faker');
 
 const mockUser = getFakeUser();
 const mockPhoto = getFakePhoto();
 const mockRound = getFakeRound(mockPhoto.id);
+const mockOngoingRound = getFakeOngoingRound(mockPhoto.id);
 
 jest.mock('../models/queries', () => {
   return {
@@ -14,6 +20,13 @@ jest.mock('../models/queries', () => {
     },
     getPhoto: () => {
       return mockPhoto;
+    },
+    getRound: (userId, photoId) => {
+      const rounds = [mockRound, mockOngoingRound];
+      const round = rounds.find(
+        (round) => round.userId === userId && round.photoId === photoId
+      );
+      return round ? round : null;
     },
     createRound: () => {
       return mockRound;
@@ -29,5 +42,32 @@ describe('/POST round route', () => {
       .post('/')
       .send({ userId: mockRound.userId, photoId: mockRound.photoId })
       .expect(201, done);
+  });
+});
+
+describe('/PUT round route', () => {
+  it("'restarts' Round when already over", () => {
+    return request(app)
+      .put(`/${mockRound.id}`)
+      .send({ userId: mockRound.userId, photoId: mockRound.photoId })
+      .type('body')
+      .then((res) => {
+        const { round } = res.body;
+        expect(round.score).toBeNull();
+      });
+  });
+
+  it('set result when Round is over', () => {
+    return request(app)
+      .put(`/${mockOngoingRound.id}`)
+      .send({
+        userId: mockOngoingRound.userId,
+        photoId: mockOngoingRound.photoId,
+      })
+      .then((res) => {
+        const { round } = res.body;
+        expect(round.score).not.toBeNull();
+        expect(round.endTime).not.toBeNull();
+      });
   });
 });
