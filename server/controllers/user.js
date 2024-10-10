66738d3b784e9d85c6472b3f00eb9cbd586aa0ea
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-10 10:47:32 +0200

    feat: add getUser function

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,11 @@
-const getUser = () => {};
+const prisma = require('../models/queries');
+
+const getUser = async (req, res) => {
+  const userId = req.params.userId;
+  const user = await prisma.getUser(userId);
+  if (!user) return res.sendStatus(404);
+  res.json(user);
+};
 
 const postUser = () => {};
 
