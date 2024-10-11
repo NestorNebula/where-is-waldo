dc7579ab2d582237e4aa91db009e17407b8738c2
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-11 16:59:54 +0200

    feat: add fetch functions and getFetchOptions

---
+++ client/src/helpers/fetch.js
@@ -0,0 +1,38 @@
+const getFetchOptions = (method, body) => {
+  return {
+    body: body ? JSON.stringify(body) : {},
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method,
+    mode: 'cors',
+  };
+};
+
+const promiseFetch = ({ url, options }) => {
+  return fetch(url, getFetchOptions(options.method, options.body))
+    .then((response) => {
+      if (response.status >= 400) {
+        throw new Error('Error when fetching levels.');
+      }
+      return response.json();
+    })
+    .then((response) => {
+      return response;
+    })
+    .catch(() => {
+      return {};
+    });
+};
+
+const asyncFetch = async ({ url, options }) => {
+  const response = await fetch(
+    url,
+    getFetchOptions(options.method, options.body)
+  );
+  if (response.status >= 400) return {};
+  const result = await response.json();
+  return result;
+};
+
+export { getFetchOptions, promiseFetch, asyncFetch };
