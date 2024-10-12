72ea76aa2cdd35a3587bc20d706706845dc03732
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-12 12:15:17 +0200

    feat: add faker functions for testing

---
+++ client/src/helpers/faker.js
@@ -0,0 +1,37 @@
+import { faker } from '@faker-js/faker';
+
+const getFakeLevel = () => {
+  const levelId = faker.number.int({ max: 10 });
+  return {
+    id: levelId,
+    title: faker.image.urlPlaceholder(),
+    characters: [
+      getFakeCharacterOnLevel(levelId),
+      getFakeCharacterOnLevel(levelId),
+      getFakeCharacterOnLevel(levelId),
+      getFakeCharacterOnLevel(levelId),
+      getFakeCharacterOnLevel(levelId),
+    ],
+  };
+};
+
+const getFakeCharacterOnLevel = (levelId) => {
+  const characterId = faker.number.int({ max: 100 });
+  return {
+    characterId,
+    coordinates: {},
+    photoId: levelId,
+    character: getFakeCharacter(characterId),
+  };
+};
+
+const getFakeCharacter = (id) => {
+  const name = faker.person.firstName();
+  return {
+    id,
+    name,
+    avatar: faker.image.urlPlaceholder({ text: name }),
+  };
+};
+
+export { getFakeLevel, getFakeCharacterOnLevel, getFakeCharacter };
