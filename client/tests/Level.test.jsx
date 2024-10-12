af8244986eed9a9fb3e4538e3b527b8a3b21df3d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-12 14:31:18 +0200

    test: add test for Level component

---
+++ client/tests/Level.test.jsx
@@ -0,0 +1,41 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Level from '../src/components/game/level/Level';
+import { mockContext } from './mocks/mocks';
+
+const fakeContext = mockContext();
+
+vi.mock('react', async () => {
+  const actual = await vi.importActual('react');
+  return {
+    ...actual,
+    useContext: () => fakeContext,
+  };
+});
+
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useParams: () => {
+      return {
+        levelId: fakeContext.levels[0].id,
+      };
+    },
+  };
+});
+
+beforeEach(() => {
+  render(
+    <MemoryRouter>
+      <Level />
+    </MemoryRouter>
+  );
+});
+
+describe('Level', () => {
+  it('renders level without error', () => {
+    expect(screen.queryByRole('img')).not.toBeNull();
+  });
+});
