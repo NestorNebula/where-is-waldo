022207912eff96b5ecba5ad061761a495f21bf9b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-12 11:06:01 +0200

    feat: add loader to App route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
-import Navbar from './components/page/navbar';
+import { useLoaderData } from 'react-router-dom';
+import Navbar from './components/page/navbar/Navbar';
 import './App.css';
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
+  const { user, levels } = useLoaderData();
   return (
     <main>
       <Navbar />
--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,9 +1,11 @@
 import App from '../App';
+import loader from '../helpers/loader';
 
 const routes = [
   {
     path: '/',
     element: <App />,
+    loader: async () => await loader(),
   },
 ];
 
