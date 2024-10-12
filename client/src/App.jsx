34ea08c83b2981f05bc9741eca65a68d503f2405
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-12 11:22:52 +0200

    feat: add context for the App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
+import { createContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Navbar from './components/page/navbar/Navbar';
 import './App.css';
 const API_URL = import.meta.env.VITE_API_URL;
 
+const GameContext = createContext({
+  user: {},
+  levels: [],
+  API_URL: null,
+});
+
 function App() {
   const { user, levels } = useLoaderData();
   return (
-    <main>
+    <GameContext.Provider value={{ user, levels, API_URL }}>
       <Navbar />
-    </main>
+      <main></main>
+    </GameContext.Provider>
   );
 }
 
