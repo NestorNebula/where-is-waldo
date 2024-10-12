f3d979ccbc3cfeeaea8dc83c85a357e9cb4859a5
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-12 14:43:37 +0200

    feat: add content to Level component

--- client/src/components/game/level/Level.jsx
+++ client/src/components/game/level/Level.jsx
@@ -1,3 +1,33 @@
-function Level() {}
+import { useContext, useState, useEffect } from 'react';
+import { GameContext } from '../../../context/GameContext';
+import { useParams } from 'react-router-dom';
+
+function Level() {
+  const [level, setLevel] = useState(null);
+  const { levels } = useContext(GameContext);
+  const { levelId } = useParams();
+
+  useEffect(() => {
+    setLevel(levels.find((lvl) => lvl.id === levelId));
+  }, [levels, levelId]);
+
+  return (
+    <>
+      {level ? (
+        <>
+          <header>{`Level ${level.id}`}</header>
+          <section>
+            <img src={`../../assets/images/${level.title}`} alt="Level Image" />
+          </section>
+        </>
+      ) : (
+        <>
+          <header>Error</header>
+          <section>{"The level doesn't exist."}</section>
+        </>
+      )}
+    </>
+  );
+}
 
 export default Level;
