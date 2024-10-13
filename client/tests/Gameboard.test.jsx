02e38eea78ed068c7466e85c6701c841b31a862a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-13 09:55:38 +0200

    feat: add test for Gameboard component

--- client/tests/Gameboard.test.jsx
+++ client/tests/Gameboard.test.jsx
@@ -1,5 +1,6 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import { mockContext } from './mocks/mocks';
 import Gameboard from '../src/components/game/gameboard/Gameboard';
@@ -26,6 +27,15 @@ describe('Gameboard', () => {
   it('renders img and characters properly', () => {
     expect(screen.queryByRole('img', { name: /level/i })).not.toBeNull();
     expect(screen.queryAllByAltText(/character/i).length).toBe(
+      fakeContext.levels[0].characters.length
+    );
+  });
+
+  it('waits for character choice after click on image', async () => {
+    const user = userEvent.setup();
+    const image = screen.getByRole('img', { name: /level/i });
+    await user.click(image);
+    expect(screen.queryAllByRole('button', { name: /choose/i }).length).toBe(
       fakeContext.levels[0].characters.length
     );
   });
