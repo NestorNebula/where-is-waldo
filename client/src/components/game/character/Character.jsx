e97d44f23120274fb00bf9a1d4ddda0d173aa2f7
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-13 15:33:44 +0200

    feat: display Character depending on game state

--- client/src/components/game/character/Character.jsx
+++ client/src/components/game/character/Character.jsx
@@ -1,16 +1,32 @@
 import PropTypes from 'prop-types';
 
-function Character({ character }) {
+function Character({ character, gameStatus }) {
   return (
-    <div>
-      <div>{character.name}</div>
-      <img src={`../../assets/images/${character.avatar}`} alt="Character" />
-    </div>
+    <>
+      {gameStatus !== 'wait' ? (
+        <div>
+          <div>{character.name}</div>
+          <img
+            src={`../../assets/images/${character.avatar}`}
+            alt="Character"
+          />
+        </div>
+      ) : (
+        <button aria-label="Choose character">
+          <div>{character.name}</div>
+          <img
+            src={`../../assets/images/${character.avatar}`}
+            alt="Character"
+          />
+        </button>
+      )}
+    </>
   );
 }
 
 Character.propTypes = {
   character: PropTypes.object.isRequired,
+  gameStatus: PropTypes.string.isRequired,
 };
 
 export default Character;
