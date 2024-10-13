94727520bfd7ae5d72d255cd5c589326f4abd437
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-13 17:02:48 +0200

    test: add tests for useGame

--- client/tests/useGame.test.jsx
+++ client/tests/useGame.test.jsx
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import { renderHook, act } from '@testing-library/react';
 import { useGame } from '../src/hooks/useGame';
-import { getFakeLevel } from '../src/helpers/faker';
+import { getFakeLevel, getFakeCoordinates } from '../src/helpers/faker';
 
 const level = getFakeLevel();
 
@@ -12,8 +12,41 @@ describe('handleImageClick', () => {
     });
     expect(result.current.gameState).toBe('on');
     act(() => {
-      result.current.handleImageClick();
+      result.current.handleImageClick({});
     });
     expect(result.current.gameState).toBe('wait');
+  });
+});
+
+describe('handleCharacterClick', () => {
+  const { result } = renderHook(() => {
+    return useGame(level.characters);
+  });
+
+  it('set state back to on after image click', () => {
+    const fakeCoordinates = getFakeCoordinates();
+    act(() => {
+      result.current.handleImageClick({
+        offsetX: fakeCoordinates.minX,
+        offsetY: fakeCoordinates.maxY,
+      });
+      result.current.handleCharacterClick(level.characters[0].characterId);
+    });
+    expect(result.current.gameState).toBe('on');
+  });
+
+  it('handles wrong click', () => {
+    expect(result.current.charactersFound[0].found).toBeFalsy();
+  });
+
+  it('handles correct click', () => {
+    act(() => {
+      result.current.handleImageClick({
+        offsetX: level.characters[0].coordinates.minX + 0.5,
+        offsetY: level.characters[0].coordinates.minY + 0.5,
+      });
+      result.current.handleCharacterClick(level.characters[0].characterId);
+    });
+    expect(result.current.charactersFound[0].found).toBeTruthy();
   });
 });
