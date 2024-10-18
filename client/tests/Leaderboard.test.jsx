966492fd352541ae73be9b99d95909d18a6ca5f1
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-18 15:58:06 +0200

    test: add tests for Leaderboard component

---
+++ client/tests/Leaderboard.test.jsx
@@ -0,0 +1,63 @@
+import { beforeAll, describe, expect, it, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import { mockContext } from './mocks/mocks';
+import { getFakeRound, getFakeOverRound } from '../src/helpers/faker';
+import Leaderboard from '../src/components/leaderboard/Leaderboard';
+
+const fakeContext = mockContext();
+const fakeRounds = [
+  [getFakeRound(10, 1), getFakeOverRound(11, 1)],
+  [getFakeOverRound(fakeContext.user.id, 2)],
+  [getFakeOverRound(15, 3), getFakeOverRound(16, 3)],
+];
+
+vi.mock('react', async () => {
+  const actual = await vi.importActual('react');
+  return {
+    ...actual,
+    useContext: () => fakeContext,
+  };
+});
+
+vi.mock('reac-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useLoaderData: () => {
+      return {
+        rounds: fakeRounds,
+      };
+    },
+  };
+});
+
+beforeAll(() => {
+  render(
+    <MemoryRouter>
+      <Leaderboard />
+    </MemoryRouter>
+  );
+});
+
+describe('Leaderboard', () => {
+  it('renders Leaderboard without error', () => {
+    expect(screen.queryByText(/error/i)).toBeNull();
+  });
+
+  it('renders Leaderboard without unfinished rounds', () => {
+    expect(screen.queryAllByText(/seconds/).length).toBe(1);
+  });
+
+  it('changes level when clicking on navigation button', async () => {
+    const user = userEvent.setup();
+    const navButton = screen.queryByRole('button', { name: /next/ });
+    await user.click(navButton);
+    expect(screen.queryAllByAltText(/seconds/).length).toBe(2);
+  });
+
+  it("displays user's score differently", () => {
+    expect(screen.queryByText('You')).not.toBeNull();
+  });
+});
