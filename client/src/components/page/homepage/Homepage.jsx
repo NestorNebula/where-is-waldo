17af4e0a960a7094541c90b363221100248572ac
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-18 15:11:09 +0200

    feat: update user when creating new round
    Avoid reloading the page and forcing the user to click again

--- client/src/components/page/homepage/Homepage.jsx
+++ client/src/components/page/homepage/Homepage.jsx
@@ -1,40 +1,37 @@
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GameContext } from '../../../context/GameContext';
-import { asyncFetch, promiseFetch } from '../../../helpers/fetch';
+import { promiseFetch } from '../../../helpers/fetch';
 
 function Homepage() {
-  const { user, levels, API_URL } = useContext(GameContext);
+  const { user, setUser, levels, API_URL } = useContext(GameContext);
   const navigate = useNavigate();
-  console.log(user.rounds);
 
   const createRound = async (levelId) => {
     const savedRound = user.rounds.find((r) => r.photoId === levelId);
     if (savedRound) {
       return navigate(`/levels/${levelId}`);
     } else {
-      const actualUser = await asyncFetch({
-        url: `${API_URL}/users/${user.id}`,
-        options: { method: 'get', body: null },
-      });
-      const updatedRound = actualUser.rounds.find((r) => r.photoId === levelId);
-      console.log(updatedRound);
-      if (updatedRound) {
-        window.location.reload();
-      } else {
-        promiseFetch({
-          url: `${API_URL}/rounds`,
-          options: {
-            method: 'post',
-            body: {
-              userId: user.id,
-              photoId: levelId,
-            },
+      promiseFetch({
+        url: `${API_URL}/rounds`,
+        options: {
+          method: 'post',
+          body: {
+            userId: user.id,
+            photoId: levelId,
           },
-        }).then(() => {
+        },
+      }).then(() => {
+        promiseFetch({
+          url: `${API_URL}/users/${user.id}`,
+          options: { method: 'get', body: null },
+        }).then((updatedUser) => {
+          if (Object.keys(updatedUser).length) {
+            setUser(updatedUser);
+          }
           navigate(`/levels/${levelId}`);
         });
-      }
+      });
     }
   };
   return (
