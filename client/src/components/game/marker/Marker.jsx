369e0f821baf152c8c5138d47430b1ae2679bc15
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-19 18:23:52 +0200

    feat: update and add styles to Marker component

--- client/src/components/game/marker/Marker.jsx
+++ client/src/components/game/marker/Marker.jsx
@@ -1,11 +1,28 @@
 import PropTypes from 'prop-types';
 
-function Marker({ coordinates }) {
-  return <>{coordinates && <div></div>}</>;
+function Marker({ coordinates, found }) {
+  return (
+    <>
+      {found && (
+        <div
+          style={{
+            position: 'absolute',
+            top: `calc(${coordinates.exactY}% - 2.5rem)`,
+            left: `calc(${coordinates.exactX}% - 2.5rem)`,
+            width: '5rem',
+            height: '5rem',
+            background: 'radial-gradient(circle, transparent 60%, #4ade80 60%)',
+            borderRadius: '100%',
+          }}
+        ></div>
+      )}
+    </>
+  );
 }
 
 export default Marker;
 
 Marker.propTypes = {
   coordinates: PropTypes.object,
+  found: PropTypes.bool,
 };
